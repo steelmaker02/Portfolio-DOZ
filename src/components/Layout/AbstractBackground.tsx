b16defbe201a5782aea53fdeaea4d3cb251a76aa
steelmaker02 <hurtszaiats@gmail.com>
2026-07-07 19:03:34 +0200

    Fix

--- src/components/Layout/AbstractBackground.tsx
+++ src/components/Layout/AbstractBackground.tsx
@@ -115,7 +115,12 @@ const AbstractBackground: React.FC = () => {
                 </>
             )}
 
-            <div className="absolute inset-0 bg-[url('https://grainy-gradients.vercel.app/noise.svg')] opacity-20 brightness-100 contrast-150 mix-blend-overlay"></div>
+            <div
+                className="absolute inset-0 opacity-20 brightness-100 contrast-150 mix-blend-overlay"
+                style={{
+                    backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 250 250' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`
+                }}
+            ></div>
 
         </div>
     );
